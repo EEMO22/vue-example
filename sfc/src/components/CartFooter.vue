9c91a165852ba561329aaf3a6b2c1bf1e24bc2fa
강호진 <battlemech@naver.com>
2021-09-15 14:31:46 +0900

    production build

---
+++ sfc/src/components/CartFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>&copy; Bit Academy</p>
+</template>
+
+<script>
+export default {}
+
+</script>
+
+<style scoped>
+    p {
+        font-style: italic;
+        font-weight: bold;
+    }
+</style>
